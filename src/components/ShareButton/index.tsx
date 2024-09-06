68b5725318e3d45075583f72ef801ce93283d981
ed <anhhoangit96@gmail.com>
2024-09-06 14:03:25 +0700

    fix; re-open share api for firefox to test

--- src/components/ShareButton/index.tsx
+++ src/components/ShareButton/index.tsx
@@ -44,48 +44,53 @@ async function urlToPngBlob(url: string): Promise<Blob> {
 
 const ShareButton: React.FC<Props> = ({ children, images, ...rest }) => {
   const [isSharing, setIsSharing] = useState(false);
-  const handleShare = async () => {
-    if (navigator.share && !(/Firefox/.test(navigator.userAgent))) {
-      try {
-        setIsSharing(true);
-        const files = await Promise.all(
-          images.map(({ url }, index) =>
-            urlToFile(url, `image${index + 1}.jpg`, "image/jpeg")
-          )
-        );
-        await navigator.share({
-          files: files,
-          title: "Shared Images",
-          text: "Check out these images!",
-        });
-      } catch (error) {
-        console.error("Failed to share images:", error);
-        alert(error);
-      } finally {
-        setIsSharing(false);
-      }
+  const isFirefox = /Firefox/.test(navigator.userAgent);
+  const isDesktop = window.matchMedia("(hover: hover)").matches;
+  const shareApiSupported = !!navigator.share;
+
+  const handleShareWithoutShareApi = async () => {
+    if (images.length === 1) {
+      // because clipboard API only supports PNG
+      const pngBlob = await urlToPngBlob(images[0].url);
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          "image/png": pngBlob,
+        }),
+      ]);
+      alert("Image converted to PNG and copied to clipboard");
     } else {
-      try {
-        setIsSharing(true);
-        if (images.length === 1) {
-          const pngBlob = await urlToPngBlob(images[0].url);
-          await navigator.clipboard.write([
-            new ClipboardItem({
-              "image/png": pngBlob,
-            }),
-          ]);
-          alert("Image converted to PNG and copied to clipboard");
-        } else {
-          const imageLinks = images.map(({ url }) => url).join('\n');
-          await navigator.clipboard.writeText(imageLinks);
-          alert("Image links copied to clipboard");
-        }
-      } catch (error) {
-        console.error("Failed to copy to clipboard:", error);
-        alert("Failed to copy to clipboard");
-      } finally {
-        setIsSharing(false);
+      const imageLinks = images.map(({ url }) => url).join("\n");
+      await navigator.clipboard.writeText(imageLinks);
+      alert("Image links copied to clipboard");
+    }
+  };
+
+  const handleShareWithShareApi = async () => {
+    const files = await Promise.all(
+      images.map(({ url }, index) =>
+        urlToFile(url, `image${index + 1}.jpg`, "image/jpeg")
+      )
+    );
+    await navigator.share({
+      files: files,
+      title: "Shared Images",
+      text: "Check out these images!",
+    });
+  };
+
+  const handleShare = () => {
+    setIsSharing(true);
+    try {
+      if (shareApiSupported) {
+        handleShareWithShareApi();
+      } else {
+        handleShareWithoutShareApi();
       }
+    } catch (error) {
+      console.error("Failed to share", error);
+      alert((error as Error).message);
+    } finally {
+      setIsSharing(false);
     }
   };
 
